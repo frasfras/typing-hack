59b7d315b80e170d1fdee1c4c9eeb21653617559
frasfras <65541080+frasfras@users.noreply.github.com>
2021-02-19 11:31:34 +0800

    Add files via upload

---
+++ resources/config.js
@@ -0,0 +1,11 @@
+
+module.exports =
+    config = {
+        typingDNA: {
+            apiKey: '71e809752a1d036eef280a8fea9ecad0',
+            apiSecret: '0679bdddad49e4b932633bfd901e6258',
+            apiServer: 'api.typingdna.com',
+        },
+        someprivatekey: '@2mws9~&%?+RN',
+        sessionSecret: 'asdkjkfh63ryfsdgcqg87w4rf7gasydi',
+    };
